0fc153641578338c7232a2ad707f3a9f770f99ef
Alexandr <olexandrzhezheria@gmail.com>
2022-07-03 10:21:51 +0300

    Проба

--- 1.py
+++ 1.py
@@ -2,3 +2,12 @@
 Создайте класс, описывающий автомобиль. Какие атрибуты и методы должны быть полностью инкапсулированы?
 Доступ к таким атрибутам и изменение данных реализуйте через специальные методы (get, set).
 """
+
+
+class Car:
+    def __int__(self, company, model, color, year, price):
+        self.company = company
+        self.model = model
+        self.__color = color
+        self.year = year
+        self.__price = price
