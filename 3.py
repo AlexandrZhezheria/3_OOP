1e08721d24e2713d07199e4dcb884658bf5c4e84
Alexandr <olexandrzhezheria@gmail.com>
2022-07-03 19:08:24 +0300

    Проба

--- 3.py
+++ 3.py
@@ -3,32 +3,60 @@
 Ознакомьтесь с декоратором property в Python. Создайте класс, описывающий температуру и позволяющий задавать и получать
 температуру по шкале Цельсия и Фаренгейта, причём данные могут быть заданы в одной шкале, а получены в другой.
 """
-class Term:
-    def __int__(self, cels, faring):
-        self.cels = cels
-        self.faring = faring
+# class Term:
+#     def __int__(self, cels, faring):
+#         self.cels = cels
+#         self.faring = faring
+#
+#
+#         def convert(c):
+#
+#             tf = (9/5)*c+32
+#
+#             return tf
+#
+#         while True:
+#
+#             tc = input('t v celciax: ')
+#             if tc == '':
+#
+#                 break
+#
+#             else:
+#
+#                 tc = int(tc)
+#
+#             tf = convert(tc)
+#
+#          print('t в цельсиях', tc, 'т в фаренгейтах', tf)
+
+class Temperature:
+    def __init__(self, value, system):
+        if system == 'f':
+            self.__f = self.__f_to_c(int(value))
+        elif system == 'c':
+            self.__c = self.__c_to_f(int(value))
 
 
-        def convert(c):
+    def __repr__(self):
+        return "Temperature <{}>".format(self.__k)
 
-            tf = (9/5)*c+32
 
-            return tf
+    def __с_to_f(c):
+        """Перевод из цельсия в фаренгейт"""
+        return (9/5) * c + 32
 
-        while True:
 
-            tc = input('t v celciax: ')
-            if tc == '':
+    def __f_to_c(f):
+        """Перевод из фаренгейт в цельсий"""
+        return 5.0*(f - 32) / 9
 
-                break
 
-            else:
 
-            tc = int(tc)
-
-            tf = convert(tc)
-
-         print('t в цельсиях', tc, 'т в фаренгейтах', tf)
+if __name__ == "__main__":
+    t = Temperature(0, 'f')
+    t.get_value()
+    print(repr(t))
 
 
 
