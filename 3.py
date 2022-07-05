23061a49326ede0c2da53932987f87a31491d938
Alexandr <olexandrzhezheria@gmail.com>
2022-07-05 21:17:44 +0300

    Проба3

--- 3.py
+++ 3.py
@@ -3,54 +3,51 @@
 Ознакомьтесь с декоратором property в Python. Создайте класс, описывающий температуру и позволяющий задавать и получать
 температуру по шкале Цельсия и Фаренгейта, причём данные могут быть заданы в одной шкале, а получены в другой.
 """
-# class Term:
-#     def __int__(self, cels, faring):
-#         self.cels = cels
-#         self.faring = faring
-#
-#
-#         def convert(c):
-#
-#             tf = (9/5)*c+32
-#
-#             return tf
-#
-#         while True:
-#
-#             tc = input('t v celciax: ')
-#             if tc == '':
-#
-#                 break
-#
-#             else:
-#
-#                 tc = int(tc)
-#
-#             tf = convert(tc)
-#
-#          print('t в цельсиях', tc, 'т в фаренгейтах', tf)
+
 
 class Temperature:
     def __init__(self, value, system):
+        self.__value = value
+        self.__system = system
         if system == 'f':
-            __f = self.f_to_c(value)
+            f = self.f_to_c(value)
         elif system == 'c':
-            __c = self.c_to_f(value)
+            c = self.c_to_f(value)
 
     def с_to_f(self):
         """Перевод из цельсия в фаренгейт"""
-        return (9/5) * self.__c + 32
+        return (9/5) * self.c + 32
 
     def f_to_c(self):
         """Перевод из фаренгейт в цельсий"""
-        return 5.0*(self.__f - 32) / 9
+        return 5.0*(self.f - 32) / 9
+
+t = Temperature(5, 'f')
 
 
 
-if __name__ == "__main__":
 
-    t = Temperature(5, 'f')
 
+
+
+
+
+
+
+# class Temperature():
+#     def __init__(self, value, system):
+#         self.__value = value
+#         self.__system = system
+#
+#
+#     def getvalue(self): # получение значения атрибута
+#         return self.__value
+#
+#     def setvalue(self, value): # установка значения атрибута
+#         self.__value = value
+#
+#
+#     value = property(getvalue, setvalue,)
 
 
 
